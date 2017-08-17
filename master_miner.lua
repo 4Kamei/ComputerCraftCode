c6b4a9cc86528c50d1adace10064c83d658d5880
Aleksander Kaminski <theinfamouspig@gmail.com>
2017-08-17 12:12:42 +0100

    Smaller gps cube for debug

--- master_miner.lua
+++ master_miner.lua
@@ -7,11 +7,12 @@ function setup_gps_cube(x, y, z)
   turtle.drop()
   turtle.up()
   local positions = {}
-  positions[1] = {20, 10, -20}
-  positions[2] = {20, 10, 20}
-  positions[3] = {-20, 10, -20}
-  positions[4] = {-20, 10, 20}
-  positions[5] = {0, 20, 0}
+  positions[1] = {2, 5, -2}
+  positions[2] = {2, 5, 2}
+  positions[3] = {-2, 5, -2}
+  positions[4] = {-2, 5, 2}
+  positions[5] = {0, 7, 0}
+  ids = fs.open("gpshosts", "w")
   for i,v in ipairs(positions) do
     turtle.select(3)
     turtle.place()
@@ -23,7 +24,10 @@ function setup_gps_cube(x, y, z)
     data["to_x"] = v[1]
     data["to_y"] = v[2]
     data["to_z"] = v[3]
-    data["id"] = peripheral.call("front", "getID")
+    local id = peripheral.call("front", "getID")
+    data["id"] = id
+    ids.write(id)
+    ids.write("\n")
     turtle.select(4)
     turtle.drop(1)
     print("turning on")
@@ -37,6 +41,7 @@ function setup_gps_cube(x, y, z)
     until result == true
     sleep(1.5)
   end
+  ids.close()
   turtle.down()
   turtle.select(2)
   turtle.suck()
@@ -54,6 +59,7 @@ local x = 0
 local y = 0
 local z = 0
 local dir = 1
+
 if args[1] == "auto" then
   x = args[2]
   y = args[3]

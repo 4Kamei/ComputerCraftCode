1f0cac29252f5f29ade54af7c853c214d4032e68
4Kamei <theinfamouspig@gmail.com>
2017-08-19 21:09:01 +0100

    No Confirmation from broadcast

--- CTMP.lua
+++ CTMP.lua
@@ -53,7 +53,9 @@ function listen(interface, channel, timeout)
       if p2 == channel then
         data = textutils.unserialize(p4)
         if data["isBroadcast"] or data["id"] == os.getComputerID() then
-          interface.transmit(channel, p3, "OK")
+          if data["isBroadcast"] == false then
+            interface.transmit(channel, p3, "OK")
+          end
           interface.close(channel)
           return true, data["data"], data["size"]
         end
